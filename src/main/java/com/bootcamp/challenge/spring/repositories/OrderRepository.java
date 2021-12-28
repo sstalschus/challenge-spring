728919bf4b48576a75fc0fa7d3eb69f5c78ec918
Jefferson Froes <jefferson.froes@mercadolivre.com>
2021-12-28 17:21:07 -0300

    Feat: Add - Repositories (Product/Order)

---
+++ src/main/java/com/bootcamp/challenge/spring/repositories/OrderRepository.java
@@ -0,0 +1,25 @@
+package com.bootcamp.challenge.spring.repositories;
+
+import java.util.List;
+
+public class OrderRepository implements Repository<Order>{
+    @Override
+    public void create(Order order) {
+
+    }
+
+    @Override
+    public void delete(Order order) {
+
+    }
+
+    @Override
+    public void update(Order order) {
+
+    }
+
+    @Override
+    public List<Order> list(Order order) {
+        return null;
+    }
+}
---
+++ src/main/java/com/bootcamp/challenge/spring/repositories/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.bootcamp.challenge.spring.repositories;
+
+import java.util.List;
+
+public class ProductRepository implements Repository <Product>{
+    @Override
+    public void create(Product product) {
+
+    }
+
+    @Override
+    public void delete(Product product) {
+
+    }
+
+    @Override
+    public void update(Product product) {
+
+    }
+
+    @Override
+    public List<Product> list(Product product) {
+        return null;
+    }
+}
