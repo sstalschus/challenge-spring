0bf8ff0f2d31d3d11538b6a655836483e9d64de9
Daniel Henrique Jorge Ramos <daniel.jramos@mercadolivre.com>
2021-12-28 17:23:12 -0300

    Feat: Create entities Order

---
+++ src/main/java/com/bootcamp/challenge/spring/entities/Order.java
@@ -0,0 +1,21 @@
+package com.bootcamp.challenge.spring.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Order {
+    private Integer id;
+    private BigDecimal totalValue;
+    private List<Product> productList;
+
+}
