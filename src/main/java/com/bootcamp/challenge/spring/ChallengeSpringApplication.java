5818eaf751dca6cb28ab08e18ac82ac9a575c24d
Samuel <samuel.stalschus@mercadolivre.com>
2021-12-28 14:51:05 -0300

    Feat: Add - initial commit

---
+++ src/main/java/com/bootcamp/challenge/spring/ChallengeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.challenge.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/challenge/spring/ChallengeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.challenge.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
