0801f8a3e500990e8e762ee455d89ae495f8790f
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2021-12-29 11:57:38 -0300

    removeido throw do metodo main

--- src/main/java/com/bootcamp/challenge/spring/ChallengeSpringApplication.java
+++ src/main/java/com/bootcamp/challenge/spring/ChallengeSpringApplication.java
@@ -1,19 +1,14 @@
 package com.bootcamp.challenge.spring;
 
-import com.bootcamp.challenge.spring.enums.SortType;
-import com.bootcamp.challenge.spring.utils.ShortStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class ChallengeSpringApplication {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) {
         SpringApplication.run(ChallengeSpringApplication.class, args);
-        SortType sortType = SortType.valueOf(1);
-        ShortStrategy.valueOf(sortType.name()).sort(new ArrayList<>());
     }
 
 }
