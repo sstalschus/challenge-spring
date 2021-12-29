0d4a0eaadeb4519284c0d4c17be2de19e80f9317
Daniel Henrique Jorge Ramos <daniel.jramos@mercadolivre.com>
2021-12-29 19:03:19 -0300

    Feat: Update in controller Order

---
+++ src/main/java/com/bootcamp/challenge/spring/controllers/OrderController.java
@@ -0,0 +1,36 @@
+package com.bootcamp.challenge.spring.controllers;
+
+import com.bootcamp.challenge.spring.entities.Order;
+import com.bootcamp.challenge.spring.entities.Product;
+import com.bootcamp.challenge.spring.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase-request")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping(value = "")
+    public ResponseEntity<Order> createOrder(@RequestBody List<Product> productList){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+
+//    @getMapping(value = "")
+//    public ResponseEntity<List<Order>> getAllOrders(){
+//        try{
+//            return ResponseEntity.ok
+//        }
+//    }
+}
