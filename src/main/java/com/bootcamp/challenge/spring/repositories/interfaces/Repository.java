a6d0855d6de7cd93ec5d99cdcc0c3f1a55a42bb7
Samuel <samuel.stalschus@mercadolivre.com>
2021-12-30 02:51:33 -0300

    Feat: Add - ordering strategy

--- src/main/java/com/bootcamp/challenge/spring/repositories/interfaces/Repository.java
+++ src/main/java/com/bootcamp/challenge/spring/repositories/interfaces/Repository.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 public interface Repository<T> {
-    void create(T t) throws IOException;
+    T create(T t) throws IOException;
     void delete(T t) throws IOException;
     void update(T t) throws IOException;
     List<T> list();
