66548cd3fa98806dfe2a5ba9b89e80b29eb13f24
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2021-12-30 18:40:06 -0300

    adicionado comentarios para documentacao

--- src/main/java/com/bootcamp/challenge/spring/repositories/interfaces/Repository.java
+++ src/main/java/com/bootcamp/challenge/spring/repositories/interfaces/Repository.java
@@ -3,6 +3,13 @@ package com.bootcamp.challenge.spring.repositories.interfaces;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Assinatura de metodos basicos para repositorios do sistema
+ * Esta interface é generica para que possa ser criada inumeros repositorios
+ *
+ * @param <T> - Tipo do repositorio
+ * */
+
 public interface Repository<T> {
     T create(T t) throws IOException;
     void delete(T t) throws IOException;
