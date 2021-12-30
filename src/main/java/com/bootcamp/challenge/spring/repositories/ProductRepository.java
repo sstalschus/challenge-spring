04e8e32f7ae0faf4587e7f68f61614e41c3336b8
arthuramorimmeli <96141013+arthuramorimmeli@users.noreply.github.com>
2021-12-30 18:28:25 -0300

    adicionado comentarios para documentacao nas classes de repositorios

--- src/main/java/com/bootcamp/challenge/spring/repositories/ProductRepository.java
+++ src/main/java/com/bootcamp/challenge/spring/repositories/ProductRepository.java
@@ -16,13 +16,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**Classe para realziar a persistencia e a leitura de registros no arquivo de armazenamento "orders.json"
+ *
+ *
+ * @author Arthur Amorim
+ * @author Jefferson Moreira
+ * @author Samuel Stalschus
+ *
+ * */
 @org.springframework.stereotype.Repository
 public class ProductRepository implements Repository<Product> {
-
+    /**
+     * Lista de produtos armazenadas em memoria
+     * */
     private List<Product> products = new ArrayList<>();
+    /**
+     * Objeto utilziado para realziar a serialização dos objetos e fazer a leitura e persistencia dos pedidos no arquivo json
+     * */
     private ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    /**
+     * Path do arquivo que sera utilizado para armazenar os pedidos
+     * */
     private final String PATH = "products.json";
 
+    /**
+     * Construtor que inicializa o repositorio,
+     * ele valida se precisa carregar os dados ja existentes ou criar um arquivo novo
+     * */
     public ProductRepository() throws IOException {
         try {
             loadOrderList();
@@ -31,6 +51,11 @@ public class ProductRepository implements Repository<Product> {
         }
     }
 
+    /**
+     * Adiciona uma ordem a lista de ordens e realiza a persistencia da nova lista no arquivo
+     *
+     * @param product - Produto para ser adicionado a lista e ao arquivo
+     * */
     @Override
     public Product create(Product product) throws IOException {
         product.setId(System.currentTimeMillis());
@@ -39,12 +64,22 @@ public class ProductRepository implements Repository<Product> {
         return product;
     }
 
+    /**
+     * Remove uma ordem a lista de ordens e realiza a persistencia da nova lista no arquivo
+     *
+     * @param product - Pedido para ser removido da lista e do arquivo
+     * */
     @Override
     public void delete(Product product) throws IOException {
         products.remove(product);
         saveFile();
     }
 
+    /**
+     * Atualiza uma ordem da lista de ordens e realiza a persistencia da nova lista no arquivo
+     *
+     * @param product - Product já atualziado e pronto para ser persistido
+     * */
     @Override
     public void update(Product product) throws IOException {
         for (Product element: this.products) {
@@ -61,15 +96,27 @@ public class ProductRepository implements Repository<Product> {
         saveFile();
     }
 
+    /**
+     * Retorna a lista de ordens do sistema
+     *
+     * @return List<Product> - Lista de produtos do sistema
+     * */
     @Override
     public List<Product> list() {
         return this.products;
     }
 
+    /**
+     * Realiza a escrita da lista de produtos no arquivo de armazenamento
+     * Caso o arquivo nao exista, ele cria um arquivo novo
+     **/
     private void saveFile() throws IOException {
         objectMapper.writeValue(new File(PATH), products);
     }
 
+    /**
+     * Realia a leitura dos registros armazenados no arquivo e adiciona na lista em memoria
+     * */
     private void loadOrderList() throws IOException {
         File file = new File(PATH);
         FileInputStream fileInputStream = new FileInputStream(file);
